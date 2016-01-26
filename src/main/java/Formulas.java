4a965f1b637de5e41037cb596559587f9b3961c7
Michael Odell <odelliesdeli@gmail.com>
2016-01-26 16:21:30 -0600

    Quick thread class and math class for usage later

--- src/main/java/Formulas.java
+++ src/main/java/Formulas.java
@@ -1,5 +1,5 @@
 public class Formulas {
-    
+
     public static double distance(int x1, int x2, int y1, int y2) {
         return Math.sqrt(((x2 - x1) ^ 2) + ((y2 - y1) ^ 2));
     }
---
+++ src/main/java/TimerThread.java
@@ -0,0 +1,41 @@
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class TimerThread extends Thread {
+    private Timer timer;
+    private int timerTime;
+
+    /**
+     * Default constructor for TimerThread
+     * Sets the default time for the alarm at 2 minutes
+     */
+    public TimerThread() {
+        // Sets the default time to 2 minutes
+        setTimerTime(2*60);
+    }
+
+    /**
+     * Creates a new timer object
+     * Sets the time alotment for the timer object to the time given in timeInSeconds
+     * @param timeInSeconds The time for the timer to run
+     */
+    public void setTimerTime(int timeInSeconds){
+        timer = new Timer();
+        timerTime = timeInSeconds * 1000;
+    }
+
+    /**
+     * Run method from extending thread
+     */
+    public void run() {
+
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                // TODO Set the task to time out after set interval
+            }
+        }, timerTime);
+    }
+
+
+}
