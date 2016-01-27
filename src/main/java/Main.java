4c3103a40cc857cc9be687175eaa9117763f96df
Michael Odell <odelliesdeli@gmail.com>
2016-01-27 11:32:05 -0600

    Meh

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,25 @@
 import lejos.hardware.Button;
 import lejos.hardware.lcd.LCD;
+import utils.Notifications;
 
 public class Main {
+
+    /**
+     * Main
+     * Start of program
+     * @param args
+     */
     public static void main(String[] args) {
+        //Check that the bricks are working
         LCD.clear();
         LCD.drawString("First EV3 Program", 0, 5);
         Button.waitForAnyPress();
         LCD.clear();
         LCD.refresh();
+    }
 
+    private void setup() {
+        //Testing the ready functionality
+        Notifications.ready();
     }
 }
--- src/main/java/utils/Notifications.java
+++ src/main/java/utils/Notifications.java
@@ -2,8 +2,14 @@ package utils;
 
 
 import lejos.hardware.Button;
+import lejos.hardware.Sound;
 import lejos.hardware.lcd.LCD;
 
+//    0: turn off button lights
+//    1/2/3: static light green/red/yellow
+//    4/5/6: normal blinking light green/red/yellow
+//    7/8/9: fast blinking light green/red/yellow
+//    >9: same as 9
 public class Notifications {
 
     //The class will likely be used as a util class so it should not be instantiated
@@ -14,6 +20,9 @@ public class Notifications {
     /**
      * Method for startup
      * Clears the screen
+     * Displays a "Ready" message
+     * Waits for the center button to be pressed
+     * On press clears screen and returns
      */
     public static void ready() {
         LCD.clear();
@@ -22,10 +31,16 @@ public class Notifications {
         Button.waitForAnyPress();
         LCD.clear();
         LCD.refresh();
+        Button.LEDPattern(0);
     }
 
+    /**
+     * Error notification
+     * Flashes the LED to red flashing
+     * Plays an error tone
+     */
     public static void error() {
-        Button.LEDPattern(0);
-
+        Button.LEDPattern(5);
+        Sound.buzz();
     }
 }
