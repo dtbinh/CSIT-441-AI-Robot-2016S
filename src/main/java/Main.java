1bc85b6d50a6002bb77a33ebd52f2c210dd693c4
Michael Odell <odelliesdeli@gmail.com>
2016-01-28 01:32:53 -0600

    Set main to run helloWorld

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,9 +11,7 @@ public class Main {
 
 
     public static void main(String[] args) {
-//        helloWorld();
-
-        setup();
+        helloWorld();
     }
 
     private static void setup() {
