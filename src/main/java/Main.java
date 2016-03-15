a03ac9a5059bf720aa7a22cb4be0c911078ef54a
Michael Odell <odelliesdeli@gmail.com>
2016-03-15 14:44:02 -0500

    Outline for ports and what they are attached to

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,27 @@ import parts.EV3LocalBrick;
 import parts.EV3RemoteBrick;
 import utils.Notifications;
 
+/**
+ * Brick 1:
+ *      Motor A: Forward wheel
+ *      Motor B: 120 Cclockwise wheel
+ *      Motor C: 120 Clockwise wheel
+ *      Motor D: Putter arm
+ *      Sensor 1: Forward Prox
+ *      Sensor 2: 120 Clockwise Prox
+ *      Sensor 3: 120 CClockwise Prox
+ *      Sensor 4: Gyroscope
+ *
+ *  Brick 2:
+ *      Motor A: Forward color
+ *      Motor B: Downward color
+ *      Motor C:
+ *      Motor D:
+ *      Sensor 1:
+ *      Sensor 2:
+ *      Sensor 3:
+ *      Sensor 4:
+ */
 public class Main {
 
     private  EV3LocalBrick localEV3;
