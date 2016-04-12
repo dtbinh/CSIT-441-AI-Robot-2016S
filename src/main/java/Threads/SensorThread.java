c014c1ecb81529d240b129766a0adbf62e3b8b37
Michael Odell <odelliesdeli@gmail.com>
2016-04-11 19:06:22 -0500

    Broke up the calibration methods

--- src/main/java/Threads/SensorThread.java
+++ src/main/java/Threads/SensorThread.java
@@ -12,62 +12,72 @@ public class SensorThread implements Runnable {
     private static EV3ColorSensor colorSensorDownLeft;
     private static EV3ColorSensor colorSensorDownRight;
 
+    // The range that will be used when the values are converted after calibration
+    private static int valMin = 0;
+    private static int valMax = 100;
+    // Values of left sensor over red and over white
+    public static float leftRedValue;
+    public static float leftWhiteValue;
+    // Values of right sensor over red and over white
+    public static float rightRedValue;
+    public static float rightWhiteValue;
+
     private boolean statement = false;
 
     public static boolean threadStop = false;
 
     public static float colorDownIDLeft;
     public static float colorDownIDRight;
-    public static float leftRedValue;
-    public static float leftWhiteValue;
-    public static float rightRedValue;
-    public static float rightWhiteValue;
+
     public static float leftColorDiff;
     public static float rightColorDiff;
 
 
-
+    /**
+     *
+     * @param colorSensorDownLeft
+     * @param colorSensorDownRight
+     */
     public SensorThread(EV3ColorSensor colorSensorDownLeft, EV3ColorSensor colorSensorDownRight) {
         SensorThread.colorSensorDownLeft = colorSensorDownLeft;
         SensorThread.colorSensorDownRight = colorSensorDownRight;
     }
 
-    public static void calibrate() {
-        // Calibrate 1
-        System.out.println("Place left color sensor on tape");
-        Button.waitForAnyPress();
+    /**
+     *
+     */
+    public static void calibrateLeftOnRed() {
+        System.out.println("The left sensor reads: " + getLeftRedMode());
+        leftRedValue = getLeftRedMode();
 
+        System.out.println("The right sensor reads: " + getRightRedMode());
+        rightWhiteValue = getRightRedMode();
+    }
+
+    public static void calibrateRightOnRed() {
+        System.out.println("The left sensor reads: " + getLeftRedMode());
+        leftWhiteValue = getLeftRedMode();
+
+        System.out.println("The right sensor reads: " + getRightRedMode());
+        rightRedValue = getRightRedMode();
+    }
+
+    public static float getLeftRedMode() {
         SampleProvider providerLeft = colorSensorDownLeft.getRedMode();
         float[] sampleLeft = new float[1];
         providerLeft.fetchSample(sampleLeft, 0);
-        System.out.println("The left sensor reads: " + sampleLeft[0]);
-        leftRedValue = sampleLeft[0];
+        return sampleLeft[0];
+    }
 
-        SampleProvider provider = colorSensorDownRight.getRedMode();
-        float[] sample = new float[1];
-        provider.fetchSample(sample, 0);
-        System.out.println("The right sensor reads: " + sample[0]);
-        rightWhiteValue = sample[0];
+    public static float getRightRedMode() {
+        SampleProvider providerRight = colorSensorDownRight.getRedMode();
+        float[] sampleRight = new float[1];
+        providerRight.fetchSample(sampleRight, 0);
+        return sampleRight[0];
+    }
 
-        // Calibrate 2
-        System.out.println("Place right color sensor on tape");
-        Button.waitForAnyPress();
-
-        providerLeft = colorSensorDownLeft.getRedMode();
-        sampleLeft = new float[1];
-        providerLeft.fetchSample(sampleLeft, 0);
-        System.out.println("The left sensor reads: " + sampleLeft[0]);
-        leftWhiteValue = sampleLeft[0];
-
-        provider = colorSensorDownRight.getRedMode();
-        sample = new float[1];
-        provider.fetchSample(sample, 0);
-        System.out.println("The right sensor reads: " + sample[0]);
-        rightRedValue = sample[0];
-
-        leftColorDiff = leftWhiteValue - leftRedValue;
-        rightColorDiff = rightWhiteValue - rightRedValue;
-
+    public static float expandSensor(float input, float localMax, float localMin) {
+        return ((valMax - valMin)/(localMax - localMin) * (input - localMax) + valMax);
     }
 
     @Override
